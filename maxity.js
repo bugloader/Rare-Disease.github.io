48c2a7863023ded071050ecc8d0a76f578714b82
bugloader <36098023+bugloader@users.noreply.github.com>
2020-02-18 01:48:08 -0600

    update info

--- maxity.js
+++ maxity.js
@@ -2,3 +2,8 @@ function backToMenu()
 {
 	window.location.replace("https://bugloader.github.io/Rare-Disease.github.io/MainPage.html");
 }
+
+function toSchoolPage()
+{
+	window.location.replace("https://www.nordangliaeducation.com/our-schools/houston/village-school");
+}
