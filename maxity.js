5f73eac015069a1f0960005283706da9443e10b8
bugloader <36098023+bugloader@users.noreply.github.com>
2020-02-23 16:34:50 -0600

    uploaded 4 passages

--- maxity.js
+++ maxity.js
@@ -3,7 +3,32 @@ function backToMenu()
 	window.location.replace("https://bugloader.github.io/Rare-Disease.github.io/MainPage.html");
 }
 
+function toSubmited()
+{
+	window.location.replace("https://bugloader.github.io/Rare-Disease.github.io/submits.html");
+}
+
 function toSchoolPage()
 {
 	window.location.replace("https://www.nordangliaeducation.com/our-schools/houston/village-school");
+}
+
+function toALSPage()
+{
+	window.location.replace("https://bugloader.github.io/Rare-Disease.github.io/ALS.html");
+}
+
+function toCSDPage()
+{
+	window.location.replace("https://bugloader.github.io/Rare-Disease.github.io/Cat Scratch Disease.html");
+}
+
+function toRBSPage()
+{
+	window.location.replace("https://bugloader.github.io/Rare-Disease.github.io/Rabies.html");
+}
+
+function toWCPage()
+{
+	window.location.replace("https://bugloader.github.io/Rare-Disease.github.io/Whooping Cough.html");
 }
