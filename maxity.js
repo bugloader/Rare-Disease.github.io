a766277a1ba7447b7202af656dad351c66b771a5
bugloader <36098023+bugloader@users.noreply.github.com>
2020-03-30 15:10:18 -0600

    updata

--- maxity.js
+++ maxity.js
@@ -42,3 +42,38 @@ function toANSPage()
 {
 	window.location.replace("https://bugloader.github.io/Rare-Disease.github.io/Amnesia.html");
 }
+
+function toPolioPage()
+{
+	window.location.replace("https://bugloader.github.io/Rare-Disease.github.io/Polio.html");
+}
+
+function toAIOPage()
+{
+	window.location.replace("https://bugloader.github.io/Rare-Disease.github.io/African Iron Overload.html");
+}
+
+function toWanderingSpleenPage()
+{
+	window.location.replace("https://bugloader.github.io/Rare-Disease.github.io/Wandering Spleen.html");
+}
+
+function toTetanusPage()
+{
+	window.location.replace("https://bugloader.github.io/Rare-Disease.github.io/Tetanus.html");
+}
+
+function toSCAPage()
+{
+	window.location.replace("https://bugloader.github.io/Rare-Disease.github.io/Sickle Cell Anemia.html");
+}
+
+function toBotulismPage()
+{
+	window.location.replace("https://bugloader.github.io/Rare-Disease.github.io/Botulism.html");
+}
+
+function toCFPage()
+{
+	window.location.replace("https://bugloader.github.io/Rare-Disease.github.io/Cystic Fibrosis.html");
+}
